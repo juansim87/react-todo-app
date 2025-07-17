a63d312bdfc3a5946bc82643300b6871f4bce3b7
LEGACY-OF-JUANC\Usuario <juansim87@gmail.com>
2025-07-17 11:24:46 +0200

    feat(TodoItem): create todo manager for rendering items

---
+++ src/components/TodoItem/TodoItem.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import './TodoItem.css';
+
+export const TodoItem = () => {
+
+  return (
+    <div>
+      <div className='todo-item'></div>
+    </div>
+  );
+};
